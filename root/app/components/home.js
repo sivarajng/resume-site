4fdbdb0dab29186a5f51347a1cd04a004a32e59b
Sivaraj <sivaraj.nagaraj@brillio.com>
2017-07-31 22:11:14 +0530

    about me

--- root/app/components/home.js
+++ root/app/components/home.js
@@ -20,7 +20,16 @@ System.register(["@angular/core"], function (exports_1, context_1) {
             Home = __decorate([
                 core_1.Component({
                     selector: 'home',
-                    templateUrl: './app/templates/home.html'
+                    templateUrl: './app/templates/home.html',
+                    styles: ['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+                        `  ul { padding-left:20px; list-style:none; }
+li { margin-bottom:10px; }
+li:before {    
+    font-family: 'FontAwesome';
+    content: '\\f111';
+    color: #3ee6bf;
+    margin:0 5px 0 -15px;
+}`]
                 })
             ], Home);
             exports_1("Home", Home);
