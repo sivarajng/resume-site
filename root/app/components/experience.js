2466cb33b8e860a4529f712e082320e679b16c47
Sivaraj <sivaraj.nagaraj@brillio.com>
2017-08-05 22:39:10 +0530

    experience update

--- root/app/components/experience.js
+++ root/app/components/experience.js
@@ -25,16 +25,14 @@ System.register(["@angular/core"], function (exports_1, context_1) {
                             from: "Mar 2017",
                             to: "Mar 2017",
                             summary: [
-                                ,
-                                "	Working as a MEAN Stack (Node, Angular, MongoDB, Express) Developer by designing and developing both Frontend and Backend modules as well as developing API Services.",
-                                "	Managing a team of four peoples and experienced in deploying Cloud Solutions like Microsoft Azure, Heroku and IBM Bluemix.",
-                                "	Also working in compliance with Agile Methodologies through JIRA and Visual Studio Team Services.",
-                                "	Strong programming Skills in NodeJS (ECMA2016 / ES6 Standard), Core Java, Mobility, Web technologies and Scripting Languages.",
-                                "	Worked on Hybrid Mobile App Developments (Android / iOS) by leveraging high end frameworks like Ionic and React-Native."
+                                "Working as a MEAN Stack (Node, Angular, MongoDB, Express) Developer by designing and developing both Frontend and Backend modules as well as developing API Services.",
+                                "Managing a team of four peoples and experienced in deploying Cloud Solutions like Microsoft Azure, Heroku and IBM Bluemix.",
+                                "Also working in compliance with Agile Methodologies through JIRA and Visual Studio Team Services.",
+                                "Strong programming Skills in NodeJS (ECMA2016 / ES6 Standard), Core Java, Mobility, Web technologies and Scripting Languages.",
+                                "Worked on Hybrid Mobile App Developments (Android / iOS) by leveraging high end frameworks like Ionic and React-Native."
                             ],
                             achievement: [
-                                ,
-                                "Awarded with “Team Excellence“ for Jun’17– a recognition for the Teams that shown high level of performance and quality delivery.",
+                                "<b>Awarded with “Team Excellence“ for Jun’17</b>– a recognition for the Teams that shown high level of performance and quality delivery.",
                                 "Developed an in-house prototype for Task Tracker portal through cutting edge technologies which has both Server and Client side apps."
                             ]
                         },
