c53c3f0b789ac60bcac140e53e4c5e5595318c42
Sivaraj <sivaraj.nagaraj@brillio.com>
2017-08-06 09:04:02 +0530

    projects

--- root/app/components/projects.ts
+++ root/app/components/projects.ts
@@ -3,21 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'projects',
   templateUrl: './app/templates/projects.html',
-  styleUrls: ['card.css']
+  styleUrls: ['card.css'],
+  styles: [`
+  .container {
+	border:0px solid #000;
+	padding:20px;
+	overflow:hidden;
+	height:100%;
+	}
+.container span {
+	display:inline-block;
+	*float:left;
+	}
+  
+  
+  `]
 })
 
 export class Projects {
 
   projects = [
     {
-      title: "B.E. (Electronics and Communication Engineering)",
-      client: "Sona College of Technology, Salem.",
-      coreApplications: 2012,
-      teamSize: "87.70 %",
-      summary: "8.77/10",
-      responsibilities: "8.77/10"
+      name: "Senior Engineer",
+      client: "Brillio",
+      coreApplications: "Bengaluru",
+      teamSize: "Mar-2017",
+      from: "Mar-2017",
+      to: "Present",
+      summary: [
+        "Working as a MEAN Stack (Node, Angular, MongoDB, Express) Developer by designing and developing both Frontend and Backend modules as well as developing API Services."
+        , "Managing a team of four peoples and experienced in deploying Cloud Solutions like Microsoft Azure, Heroku and IBM Bluemix."
+        , "Also working in compliance with Agile Methodologies through JIRA and Visual Studio Team Services."
+        , "Strong programming Skills in NodeJS (ECMA2016 / ES6 Standard), Core Java, Mobility, Web technologies and Scripting Languages."
+        , "Worked on Hybrid Mobile App Developments (Android / iOS) by leveraging high end frameworks like Ionic and React-Native."
+      ],
+      roles: [
+        "<b>Awarded with “Team Excellence“</b> for Jun’17– a recognition for the Teams that shown high level of performance and quality delivery."
+        , "<b>Developed an in-house prototype for Task Tracker portal</b> through cutting edge technologies which has both Server and Client side apps."
+      ]
     },
-  
+
+   
 
 
 
